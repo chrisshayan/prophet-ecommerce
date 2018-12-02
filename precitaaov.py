9d9ca9f39dd50d4d9c66df9c61ec23223b4e65a2
chrisshayan <hamedshayan@gmail.com>
2018-12-02 18:13:46 +0700

    added performance_metrics and cross validation. Also fine tuned some other metrics like removing outliers

--- precitaaov.py
+++ precitaaov.py
@@ -1,24 +1,36 @@
 import pandas as pd
+
 from fbprophet import Prophet
+from fbprophet.diagnostics import cross_validation
+from fbprophet.diagnostics import performance_metrics
 
-aovDataFile = pd.read_csv('files/Mar-Nov-18-eCom-aov.csv')
-aovDataFile.head()
+dataFile = pd.read_csv('files/Mar-Nov-18-eCom-aov.csv')
+dataFile.head()
+dataFile.loc[(dataFile['ds'] == '20-10-2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '26/11/2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '27/11/2018'), 'y'] = None
 
+prophet = Prophet(
+    growth='linear',
+    seasonality_mode='additive')
 
-m = Prophet()
-m.fit(aovDataFile)
+prophet.fit(dataFile)
 
-future = m.make_future_dataframe(periods=365)
+future = prophet.make_future_dataframe(freq='D', periods=30*6)
 future.tail()
 
-forecast = m.predict(future)
+forecast = prophet.predict(future)
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-
-fig1 = m.plot(forecast)
+fig1 = prophet.plot(forecast)
 fig1.savefig('forecastAOV.png')
 
-fig2 = m.plot_components(forecast)
+fig2 = prophet.plot_components(forecast)
 fig2.savefig('forecastComponentsAOV.png')
 
+cross_validation_results = cross_validation(prophet, initial='210 days', period='15 days', horizon='70 days')
+print cross_validation_results
+
+performance_metrics_results = performance_metrics(cross_validation_results)
+print performance_metrics_results
 
--- precitasoqty.py
+++ precitasoqty.py
@@ -1,23 +1,22 @@
 import pandas as pd
-import numpy  as np
 
 from fbprophet import Prophet
 from fbprophet.diagnostics import cross_validation
-
-
-#  Take in true and predicted values then calculate MAPE
-def mean_absolute_percentage_error(y_true, y_pred):
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
+from fbprophet.diagnostics import performance_metrics
 
 dataFile = pd.read_csv('files/Mar-Nov-18-eCom-SOQty.csv')
 dataFile.head()
+dataFile.loc[(dataFile['ds'] == '20-10-2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '26/11/2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '27/11/2018'), 'y'] = None
 
-prophet = Prophet()
+prophet = Prophet(
+    growth='linear',
+    seasonality_mode='additive')
+
 prophet.fit(dataFile)
 
-future = prophet.make_future_dataframe(periods=365)
+future = prophet.make_future_dataframe(freq='D', periods=30*6)
 future.tail()
 
 forecast = prophet.predict(future)
@@ -29,6 +28,8 @@ fig1.savefig('forecastSOQty.png')
 fig2 = prophet.plot_components(forecast)
 fig2.savefig('forecastComponentsSOQty.png')
 
-cross_validation_results = cross_validation(prophet, initial='30 days', period='15 days', horizon='5 days')
-# map_baseline = mean_absolute_percentage_error(cross_validation_results, cross_validation_results.yhat)
-# print map_baseline
+cross_validation_results = cross_validation(prophet, initial='210 days', period='15 days', horizon='70 days')
+print cross_validation_results
+
+performance_metrics_results = performance_metrics(cross_validation_results)
+print performance_metrics_results
