e98713106e18cc5654fe5afdb9e430836101f0d6
chrisshayan <hamedshayan@gmail.com>
2018-12-02 11:57:18 +0700

    implemented a MAPE function, as well as included in cross validation

--- precitasoqty.py
+++ precitasoqty.py
@@ -1,24 +1,34 @@
 import pandas as pd
+import numpy  as np
+
 from fbprophet import Prophet
+from fbprophet.diagnostics import cross_validation
+
+
+#  Take in true and predicted values then calculate MAPE
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
 
 dataFile = pd.read_csv('files/Mar-Nov-18-eCom-SOQty.csv')
 dataFile.head()
 
+prophet = Prophet()
+prophet.fit(dataFile)
 
-m = Prophet()
-m.fit(dataFile)
-
-future = m.make_future_dataframe(periods=365)
+future = prophet.make_future_dataframe(periods=365)
 future.tail()
 
-forecast = m.predict(future)
+forecast = prophet.predict(future)
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-
-fig1 = m.plot(forecast)
+fig1 = prophet.plot(forecast)
 fig1.savefig('forecastSOQty.png')
 
-fig2 = m.plot_components(forecast)
+fig2 = prophet.plot_components(forecast)
 fig2.savefig('forecastComponentsSOQty.png')
 
-
+cross_validation_results = cross_validation(prophet, initial='30 days', period='15 days', horizon='5 days')
+# map_baseline = mean_absolute_percentage_error(cross_validation_results, cross_validation_results.yhat)
+# print map_baseline
