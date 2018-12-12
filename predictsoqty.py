7d2b29f66fe8c6f41a8c9c743b02abb024e8e359
chrisshayan <hamedshayan@gmail.com>
2018-12-12 10:37:43 +0700

    changed the naming

---
+++ predictsoqty.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from fbprophet import Prophet
+from fbprophet.diagnostics import cross_validation
+from fbprophet.diagnostics import performance_metrics
+
+dataFile = pd.read_csv('files/Mar-Nov-18-eCom-SOQty.csv')
+dataFile.head()
+# adding the outliers into the model
+dataFile.loc[(dataFile['ds'] == '20-10-2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '26/11/2018'), 'y'] = None
+dataFile.loc[(dataFile['ds'] == '27/11/2018'), 'y'] = None
+
+prophet = Prophet(
+    growth='linear',
+    seasonality_mode='additive')
+
+prophet.fit(dataFile)
+
+future = prophet.make_future_dataframe(freq='D', periods=30*6)
+future.tail()
+
+forecast = prophet.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+fig1 = prophet.plot(forecast)
+fig1.savefig('forecastSOQty.png')
+
+fig2 = prophet.plot_components(forecast)
+fig2.savefig('forecastComponentsSOQty.png')
+
+cross_validation_results = cross_validation(prophet, initial='210 days', period='15 days', horizon='70 days')
+print cross_validation_results
+
+performance_metrics_results = performance_metrics(cross_validation_results)
+print performance_metrics_results
